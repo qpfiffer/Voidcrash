81991b2843548bf9ddda5137b3b96f905fcde9f3
Quinlan Pfiffer <qpfiffer@gmail.com>
2019-12-06 10:54:38 -0800

    Fuckin around with shaders

--- the_black_cartograph/main.lua
+++ the_black_cartograph/main.lua
@@ -4,17 +4,29 @@ local GameState = require("src/GameState")
 local InitialState = require("src/states/InitialState")
 local Renderer = require("src/Renderer")
 
+-- Constants
 scale = 2 -- love.window.getDPIScale()
 window_width = 640 * scale
 window_height = 480 * scale
 
+-- Game internals
 local game_state = nil
 local renderer = nil
+
+-- Shader stuff
+local canvas = nil
+local shader = nil
 
 function love.load(arg)
     love.mouse.setVisible(false)
     love.window.setTitle("black_cartograph.exe")
     love.window.setMode(window_width, window_height, {resizable=false, vsync=false})
+
+    canvas = love.graphics.newCanvas()
+    local str = love.filesystem.read("assets/CRT.frag")
+    shader = love.graphics.newShader(str)
+    shader:send("inputSize", {love.graphics.getWidth(), love.graphics.getHeight()})
+    shader:send("textureSize", {love.graphics.getWidth(), love.graphics.getHeight()})
 
     local initial_state = InitialState:init()
     game_state = GameState:init(initial_state)
@@ -58,5 +70,13 @@ function love.update(dt)
 end
 
 function love.draw()
+    love.graphics.setCanvas(canvas)
+
     game_state:render_current_state(renderer)
+
+    love.graphics.setCanvas()
+    love.graphics.setShader(shader)
+    shader:send('tex0', tex)
+    love.graphics.draw(canvas)
+    love.graphics.setShader()
 end
