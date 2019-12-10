c09bd56acba4be8fdd0c8ce0a5877c05d247db83
Quinlan Pfiffer <qpfiffer@gmail.com>
2019-12-09 21:05:30 -0800

    Whatever man

--- the_black_cartograph/src/states/MenuState.lua
+++ the_black_cartograph/src/states/MenuState.lua
@@ -63,11 +63,9 @@ function MenuState:key_pressed(game_state, key)
 end
 
 function MenuState:render(renderer)
-    renderer:_draw_raw_numbers(renderer.traumae_font, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}, 8, 1)
-    renderer:_draw_raw_numbers(renderer.traumae_font, {13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24}, 9, 2)
-    renderer:_draw_raw_numbers(renderer.traumae_font, {25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36}, 10, 1)
-    renderer:_draw_raw_numbers(renderer.traumae_font, {13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24}, 11, 1)
-    renderer:_draw_raw_numbers(renderer.traumae_font, {25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36}, 12, 2)
+    renderer:draw_traumae_string("the harvest moon is", 8, 2)
+    renderer:draw_traumae_string("good for black tides", 9, 2)
+    renderer:draw_traumae_string("and a dead wind blows", 10, 2)
 
     for i=1, #menu_items do
         if i == self.current_menu_item then
