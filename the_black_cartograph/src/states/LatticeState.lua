5308c8c987da1f3993a7eec4c9a5ec18dab399b3
Quinlan Pfiffer <qpfiffer@gmail.com>
2020-11-17 09:22:51 -0700

    Lattice state

--- the_black_cartograph/src/states/LatticeState.lua
+++ the_black_cartograph/src/states/LatticeState.lua
@@ -11,7 +11,6 @@ local MAP_Y_MAX = 35
 local TICKS_ADVANCE_MIN = 1
 local TICKS_ADVANCE_MAX = 3
 
-
 local LATTICE_GRID_SIZE = 3
 
 function LatticeState:init()
@@ -22,6 +21,8 @@ function LatticeState:init()
         screen_state = {},
 
         blink_cursor_on = true,
+        selected = {1, 1},
+        select_mode = "x",
         ticks_advanced = BLINK_TICK_COUNT,
     }
     setmetatable(this, self)
@@ -29,7 +30,31 @@ function LatticeState:init()
     return this
 end
 
+function LatticeState:_ensure_grid_size_selected(idx)
+    if self.selected[idx] > LATTICE_GRID_SIZE then
+        self.selected[idx] = 0
+    elseif self.selected[idx] < 0 then
+        self.selected[idx] = LATTICE_GRID_SIZE
+    end
+end
+
 function LatticeState:key_pressed(game_state, key)
+    if key == "left" then
+        self.select_mode = "x"
+        self.selected[1] = self.selected[1] - 1
+    elseif key == "right" then
+        self.select_mode = "x"
+        self.selected[1] = self.selected[1] + 1
+    elseif key == "up" then
+        self.select_mode = "y"
+        self.selected[2] = self.selected[2] - 1
+    elseif key == "down" then
+        self.select_mode = "y"
+        self.selected[2] = self.selected[2] + 1
+    end
+
+    self:_ensure_grid_size_selected(1)
+    self:_ensure_grid_size_selected(2)
 end
 
 function LatticeState:update(game_state, dt)
@@ -50,44 +75,10 @@ function LatticeState:update(game_state, dt)
     end
 end
 
-function _draw_iso_square(renderer, x, y, z)
-    local block_width = 128
-    local block_height = 72
-
-    local y_padding = 64
-
-    -- Top
-    love.graphics.line(
-        (x - z) * block_width,
-        (y + z) * block_height + (y * y_padding),
-        (x - z + 1) * block_width,
-        (y + z) * block_height + (y * y_padding))
-    -- Left
-    love.graphics.line(
-        (x - z - 1) * block_width,
-        (y + z + 1) * block_height + (y * y_padding),
-        (x - z) * block_width,
-        (y + z) * block_height + (y * y_padding))
-    -- Right
-    love.graphics.line(
-        (x - z) * block_width,
-        (y + z + 1) * block_height + (y * y_padding),
-        (x - z + 1) * block_width,
-        (y + z) * block_height + (y * y_padding))
-    -- Bottom
-    love.graphics.line(
-        (x - z - 1) * block_width,
-        (y + z + 1) * block_height + (y * y_padding),
-        (x - z) * block_width,
-        (y + z + 1) * block_height + (y * y_padding))
-end
-
 function LatticeState:render(renderer, game_state)
     renderer:draw_traumae_string("LATTICE CONN", 1, 0)
     renderer:draw_string("Lattice Sleeper Conn.", 2, 0)
     renderer:draw_string("CONN:", 3, 0)
-
-    local selected = {2, 2, 2}
 
     local width = love.graphics.getWidth()
     local height = love.graphics.getHeight()
@@ -95,22 +86,59 @@ function LatticeState:render(renderer, game_state)
     renderer:set_color("white")
     love.graphics.translate(width/2, height/8)
     for x = 1, LATTICE_GRID_SIZE do
-        if self.blink_cursor_on and x == selected[1] then
-            renderer:set_color("red")
-        else
-            renderer:set_color("white")
-        end
         for y = 1, LATTICE_GRID_SIZE do
             for z = 1, LATTICE_GRID_SIZE do
-                _draw_iso_square(renderer, x, y, z)
+                local block_width = 128
+                local block_height = 72
+
+                local y_padding = 64
+
+                -- Top
+                if self.blink_cursor_on and self.select_mode == "y" and (z - 1) == self.selected[2] then
+                    renderer:set_color("red")
+                end
+                love.graphics.line(
+                    (x - z) * block_width,
+                    (y + z) * block_height + (y * y_padding),
+                    (x - z + 1) * block_width,
+                    (y + z) * block_height + (y * y_padding))
+                renderer:set_color("white")
+
+                if self.blink_cursor_on and self.select_mode == "x" and (x - 1) == self.selected[1] then
+                    renderer:set_color("red")
+                end
+                -- Left
+                love.graphics.line(
+                    (x - z - 1) * block_width,
+                    (y + z + 1) * block_height + (y * y_padding),
+                    (x - z) * block_width,
+                    (y + z) * block_height + (y * y_padding))
+                renderer:set_color("white")
+
+                if self.blink_cursor_on and self.select_mode == "x" and x == self.selected[1] then
+                    renderer:set_color("red")
+                end
+                -- Right
+                love.graphics.line(
+                    (x - z) * block_width,
+                    (y + z + 1) * block_height + (y * y_padding),
+                    (x - z + 1) * block_width,
+                    (y + z) * block_height + (y * y_padding))
+                renderer:set_color("white")
+
+                if self.blink_cursor_on and self.select_mode == "y" and z == self.selected[2] then
+                    renderer:set_color("red")
+                end
+                -- Bottom
+                love.graphics.line(
+                    (x - z - 1) * block_width,
+                    (y + z + 1) * block_height + (y * y_padding),
+                    (x - z) * block_width,
+                    (y + z + 1) * block_height + (y * y_padding))
+                renderer:set_color("white")
             end
         end
     end
-
-    -- if self.blink_cursor_on then
-    --     renderer:set_color("red")
-    --     _draw_iso_square(renderer, selected[1], selected[2], selected[3])
-    -- end
 
     love.graphics.origin()
 end
