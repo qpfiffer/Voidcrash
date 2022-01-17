1d3d3ea0ce696a75c1ab2cc6c1b3c6323187f21c
Quinlan Pfiffer <qpfiffer@gmail.com>
2022-01-16 19:32:51 -0700

    Selectable windows

--- src/states/HullState.lua
+++ src/states/HullState.lua
@@ -9,11 +9,17 @@ local MAP_Y_MAX = 35
 local TICKS_ADVANCE_MIN = 1
 local TICKS_ADVANCE_MAX = 3
 
+local BLINK_TICK_COUNT = 20
+
 function HullState:init(next_state)
     local this = {
-        dtotal = 0,             -- Delta time total
-        ticks_advanced = TICKS_ADVANCE_MAX,     -- Number of ticks left to go before update
+        dtotal = 0,
+        ticks_advanced = BLINK_TICK_COUNT,
         next_state = next_state,
+
+        blink_cursor_on = true,
+
+        selected_idx = 1,
     }
     setmetatable(this, self)
 
@@ -21,6 +27,17 @@ function HullState:init(next_state)
 end
 
 function HullState:key_pressed(game_state, key)
+    if key == "right" then
+        self.selected_idx = self.selected_idx + 1
+    elseif key == "left" then
+        self.selected_idx = self.selected_idx - 1
+    end
+
+    if self.selected_idx > 3 then
+        self.selected_idx = 1
+    elseif self.selected_idx < 1 then
+        self.selected_idx = 3
+    end
 end
 
 function HullState:update(game_state, dt)
@@ -29,9 +46,10 @@ function HullState:update(game_state, dt)
         self.dtotal = self.dtotal - constants.TICKER_RATE
         self.ticks_advanced = self.ticks_advanced - 1
 
-        if self.ticks_advanced > 0 then
-            -- Wait til next time.
-            return
+        if self.ticks_advanced <= 0 then
+            -- Reset the counter.
+            self.ticks_advanced = BLINK_TICK_COUNT
+            self.blink_cursor_on = not self.blink_cursor_on
         end
 
         -- Do something.
@@ -119,9 +137,25 @@ function HullState:render(renderer, game_state)
     local w = constants.MAP_X_MAX/3
     local h = constants.MAP_Y_MAX - y - 10
 
-    renderer:render_window(x, y, w - 4, h, "black", "white")
-    renderer:render_window(constants.MAP_X_MAX/3 + 1, y, w - 3, h, "black", "white")
-    renderer:render_window(2 * (constants.MAP_X_MAX/3) + 2, y, w - 3, h, "black", "white")
+    renderer:set_color("white")
+
+    local color = "white"
+    if self.blink_cursor_on and self.selected_idx == 1 then
+        color = "red"
+    end
+    renderer:render_window(x, y, w - 4, h, "black", color)
+
+    color = "white"
+    if self.blink_cursor_on and self.selected_idx == 2 then
+        color = "red"
+    end
+    renderer:render_window(constants.MAP_X_MAX/3 + 1, y, w - 3, h, "black", color)
+
+    color = "white"
+    if self.blink_cursor_on and self.selected_idx == 3 then
+        color = "red"
+    end
+    renderer:render_window(2 * (constants.MAP_X_MAX/3) + 2, y, w - 3, h, "black", color)
 
     self:_draw_power_used_pane(renderer, game_state)
     self:_draw_cargo_pane(renderer, game_state)
